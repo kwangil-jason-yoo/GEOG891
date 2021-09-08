08227353382533706449314e52bc5b6599d16ebf
Jason <yooki1117@gmail.com>
2021-09-08 15:21:36 -0500

    9.8 class

---
+++ src/9_8.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(sf)
+p.counties <- "./data/CBW/County_Boundaries.shp"
+p.stations <- "./data/CBW/Non-Tidal_Water_Quality_Monitoring_Stations_in_the_Chesapeake_Bay.shp"
+
+d.counties <- sf::read_sf(p.counties)
+d.stations <- sf::read_sf(p.stations)
+
+glimpse(d.counties)
+glimpse(d.stations)
